fb88edf4480b2e7f52e4ed0c9e9226073e828a1f
Kinice <szp93@126.com>
2020-01-03 19:06:58 +0800

    Update ua for ipad ios 13 new ua.

--- js-scripts/imqqUa.js
+++ js-scripts/imqqUa.js
@@ -1,18 +1,17 @@
-var os = function() {
-    var ua = navigator.userAgent,
-            isWindowsPhone = /(?:Windows Phone)/.test(ua),
-            isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
-            isAndroid = /(?:Android)/.test(ua),
-            isFireFox = /(?:Firefox)/.test(ua),
-            isChrome = /(?:Chrome|CriOS)/.test(ua),
-            isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
-            isPhone = /(?:iPhone)/.test(ua) && !isTablet,
-            isPc = !isPhone && !isAndroid && !isSymbian;
-    return {
-        isTablet: isTablet,
-        isPhone: isPhone,
-        isAndroid : isAndroid,
-        isPc : isPc
-    };
-}();
+const os = (() => {
+  const ua = navigator.userAgent,
+    isAndroid = /(?:Android)/.test(ua),
+    isFireFox = /(?:Firefox)/.test(ua),
+    isPad = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
+    isiPad = /(?:iPad)/.test(ua) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1), // for ios 13 ipad
+    isiPhone = /(?:iPhone)/.test(ua) && !isPad,
+    isPC = !isiPhone && !isAndroid && !isPad && !isiPad
+  return {
+    isPad,
+    isiPhone,
+    isAndroid,
+    isPC,
+    isiPad
+  }
+})()
     
